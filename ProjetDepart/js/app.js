1afaa7453a6af8fb73a5f0d627e2ff6af0cac425
Admin <lacou.alban@gmail.com>
2021-01-27 17:12:49 +0100

    stack des item dans le panier

--- ProjetDepart/js/app.js
+++ ProjetDepart/js/app.js
@@ -2,6 +2,11 @@ let buttonAdd = document.querySelectorAll('.add-to-cart');
 let panier = document.querySelector('#cart-table');
 let Allh4 = document.querySelectorAll('h4');
 let allDiscount = document.querySelectorAll('.discount');
+let allStock = document.querySelectorAll('.stock');
+
+
+
+
 
 //ajout d'un event listener sur chaque boutton
 buttonAdd.forEach(function(elem){
@@ -19,6 +24,15 @@ function clicSurBtn(e){
     
     let article = Allh4[BtnIndex-1].innerHTML
     let Price = allDiscount[BtnIndex-1].innerHTML
+    let stock = allStock[BtnIndex-1].innerHTML //recuperation de la string des stock
+    let nbStock = parseInt(stock);	//conversion en int
+    if(nbStock <= 0){
+    	createNotif(article,"stock")
+    	return;
+    }
+    nbStock--;
+
+    allStock[BtnIndex-1].innerHTML = nbStock;
     addCart(article,Price,1);
 }
 
@@ -29,7 +43,24 @@ function clicSurBtn(e){
 	Add the Article clicked to the cart
 */
 function addCart(Article, Prix, Qt){
-	let newLine = document.createElement('thead');
+	let allTh = document.querySelectorAll('th');
+	let isAlreadyBuy = false;
+	let index = 0;
+	for(let i = 0; i < allTh.length;i++){
+		console.log("1: "+Article +" 2: "+allTh[i].innerHTML)
+		if(Article == allTh[i].innerHTML){
+			isAlreadyBuy = true;
+			index = i;
+		}
+	}
+	console.log(isAlreadyBuy)
+	if(isAlreadyBuy){
+		let value = allTh[index+2].innerHTML
+		let nbValue = parseInt(value)
+		allTh[index+2].innerHTML = nbValue+1
+												//CHANGEMENT DES PRIX
+	}else{
+		let newLine = document.createElement('thead');
 	let newTr = document.createElement('tr');
 	
 	let newTh0 = document.createElement('th');
@@ -59,19 +90,31 @@ function addCart(Article, Prix, Qt){
 	newLine.appendChild(newTr);
 	newTr.style.backgroundColor = "white";
 	panier.appendChild(newLine);
-	createNotif(Article);
+	}
+
+
+
+	
+	createNotif(Article,"add");
+	
 }
 
 
-function createNotif(Article){
-	console.log(Article)
-	let div = document.querySelector('.notif')
-	let notif = document.createElement('div')
+function createNotif(Article,Text){
+	let div = document.querySelector('#notification_container')
+	let notif = document.createElement('p')
+	
 	div.appendChild(notif)
 	notif.style.backgroundColor = "cyan";
 	notif.style.marginBottom = 10+"px";
-
-	notif.innerHTML = Article+ " a été ajouté au panier"
+	if(Text == "add"){
+		notif.innerHTML = Article+ " a été ajouté au panier"
+	}else if(Text == "sup"){
+		notif.innerHTML = Article+ " a été retiré du panier"
+	}else if(Text == "stock"){
+		notif.innerHTML = "il n'a plus de disponibilité pour l'article: "+Article;
+	}
+	
 	setTimeout(function(){
     notif.style.display = "none";
 }, 3000);
